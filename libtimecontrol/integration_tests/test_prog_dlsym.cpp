b713412250189d21e11c9a085537cd3e28ca245d
William Henning <wchenningwchenning@gmail.com>
2025-06-27 14:22:15 -0400

    Fix dlsym integration test bug

--- libtimecontrol/integration_tests/test_prog_dlsym.cpp
+++ libtimecontrol/integration_tests/test_prog_dlsym.cpp
@@ -1,13 +1,20 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <cstdint>
 #include <dlfcn.h>
+#include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+
+#include <gnu/lib-names.h>
 
 int64_t kMillion = 1'000'000;
 
 int main(int, char**) {
   int (*usleep_f)(useconds_t) = nullptr;
-  void* libc_handle = dlopen("libc.so", RTLD_LAZY);
+  void* libc_handle = dlopen(LIBC_SO, RTLD_LAZY);
   usleep_f = (decltype(usleep_f))dlsym(libc_handle, "usleep");
 
   while (true) {
