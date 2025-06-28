d2000e85f42d207861210a9c3910cb06116460a7
William Henning <wchenningwchenning@gmail.com>
2025-06-28 01:05:43 -0400

    Configure meson to install ld preload libraries and test binaries into the tree

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+meson setup . build -Dlibdir=$SCRIPT_DIR/libtimecontrol/lib -Dbindir=$SCRIPT_DIR/libtimecontrol/bin
+cd build
+ninja
+meson install
+meson test
+
+cd ..
