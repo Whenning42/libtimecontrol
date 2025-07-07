008a55804cc18240fd7f5ef223bdf3a7ebb8dce3
William Henning <wchenningwchenning@gmail.com>
2025-07-07 15:21:57 -0400

    Add a release script to the meson build script

--- build.sh
+++ build.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-set -eu
+# Builds the C++ part of libtimecontrol.
+# Usage:
+#   For a debug build:
+#     $ ./build.sh
+#   For a release build:
+#     $ ./build.sh --release
 
+set -euo pipefail
+
+buildtype=debug
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --release) buildtype=release; shift ;;
+    *)         break ;;
+  esac
+done
+
+rm -rf build
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-meson setup . build -Dlibdir=$SCRIPT_DIR/libtimecontrol/lib -Dbindir=$SCRIPT_DIR/libtimecontrol/bin
+meson setup . build -Dlibdir=$SCRIPT_DIR/libtimecontrol/lib -Dbindir=$SCRIPT_DIR/libtimecontrol/bin -Dbuildtype=${buildtype}
 cd build
-ninja
+ninja -v
 meson install
 meson test --print-errorlog
