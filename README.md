58c56948f98fdedcf7ec6306c7d1a3a74d1705a6
William Henning <wchenningwchenning@gmail.com>
2025-07-08 00:54:49 -0400

    Fix typos in comments

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ from libtimecontrol import TimeController
 
 # TimeControllers run on "time channels" which are non-negative int32 values that the
 # time controlling and time controlled processes use to coordinate their inter-process
-# comminucation. All time controllers should get their own time channel. As many time
+# communication. All time controllers should get their own time channel. As many time
 # controlled processes as you desire can listen to a time channel.
 time_channel = 0
 controller = TimeController(time_channel)
@@ -35,9 +35,8 @@ controller.set_speedup(25)
 # right corner of its output, is moving.
 env = os.environ
 subprocess.run(
-    "watch -n1 date"
+    ["watch", "-n1", "date"]
     env=env | controller.child_flags(),
-    shell=True,
 )
 ```
 
--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ from libtimecontrol import TimeController
 
 # TimeControllers run on "time channels" which are non-negative int32 values that the
 # time controlling and time controlled processes use to coordinate their inter-process
-# comminucation. All time controllers should get their own time channel. As many time
+# communication. All time controllers should get their own time channel. As many time
 # controlled processes as you desire can listen to a time channel.
 time_channel = 0
 controller = TimeController(time_channel)
@@ -17,7 +17,6 @@ controller.set_speedup(25)
 #   Due to limitations in the time control implementation, the watch
 env = os.environ
 subprocess.run(
-    "watch -n1 date",
+    ["watch", "-n1", "date"],
     env=env | controller.child_flags(),
-    shell=True,
 )
