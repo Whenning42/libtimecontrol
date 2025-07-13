7876802006d5f460df101f4bfb1244f1e6167992
William Henning <wchenningwchenning@gmail.com>
2025-07-12 23:29:58 -0400

    Add a dlsym + 32-bit integration test

--- libtimecontrol/integration_tests/integration_test.py
+++ libtimecontrol/integration_tests/integration_test.py
@@ -10,13 +10,15 @@ from libtimecontrol.libtimecontrol import PreloadMode, TimeController
 from libtimecontrol.path import PACKAGE_ROOT
 
 
-def run_program(name, duration, env_vars):
+def run_program(name, duration, env_vars) -> str:
     path = PACKAGE_ROOT + "/bin/" + name
     try:
         env = os.environ
-        subprocess.run(
+        r = subprocess.run(
             path, stdout=subprocess.PIPE, timeout=duration, env=env | env_vars
         )
+        print("======== Unexpected Subprocess Return ======", r)
+        return ""
     except subprocess.TimeoutExpired as e:
         return e.stdout.decode()
 
@@ -28,14 +30,16 @@ def test_prog(name, preload_mode, speedup):
     channel = random.randint(0, 2**30)
     controller = TimeController(channel, preload_mode)
     controller.set_speedup(speedup)
+    print("============= RUNNING: ", name, " =============")
     out = run_program(name, test_length, controller.child_flags())
     out_lines = out.split("\n")
     assert (
         len(out_lines) > 0.8 * expected_ticks and len(out_lines) < 1.2 * expected_ticks
-    ), f"{len(out_lines)} {out_lines}"
+    ), f"Actual ticks: {len(out_lines)} Expected ticks: {expected_ticks}"
     print("============= PASSED: ", name, " =============")
 
 
 test_prog("test_prog", PreloadMode.REGULAR, 2),
 test_prog("test_prog32", PreloadMode.REGULAR, 3),
 test_prog("test_prog_dlsym", PreloadMode.DLSYM, 4),
+test_prog("test_prog_dlsym32", PreloadMode.DLSYM, 5),
