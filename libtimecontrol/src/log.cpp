9f952aa578fc0d653a4477124cf2e1b6c67e2bc2
William Henning <wchenningwchenning@gmail.com>
2025-07-12 23:34:26 -0400

    Add a nanoprintf based log function

---
+++ libtimecontrol/src/log.cpp
@@ -0,0 +1,54 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif // _GNU_SOURCE
+#include <errno.h>
+
+#define NANOPRINTF_USE_FIELD_WIDTH_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_PRECISION_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_LARGE_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_SMALL_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_FLOAT_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_BINARY_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_WRITEBACK_FORMAT_SPECIFIERS 1
+
+// Compile nanoprintf in this translation unit.
+#define NANOPRINTF_IMPLEMENTATION
+#include "src/third_party/nanoprintf.h"
+
+#include <cstring>
+#include <mutex>
+#include <ostream>
+#include <stdarg.h>
+#include <stdio.h>
+#include <sstream>
+#include <thread>
+#include <unistd.h>
+
+
+extern char *program_invocation__name;
+
+void log(const char* fmt, ...) {
+#ifdef DEBUG
+  static std::mutex log_mu;
+  std::lock_guard<std::mutex> l(log_mu);
+  va_list args;
+  va_start(args, fmt);
+
+  char out[512];
+  int pid = getpid();
+  int tid = gettid();
+  npf_snprintf(out, 256, "%s: p: %d, t: %d: ", program_invocation_name, pid, tid);
+
+  char body[256];
+  npf_vsnprintf(body, 254, fmt, args);
+  strncat(body, "\n", 2);
+  strncat(out, body, 256);
+
+  write(STDERR_FILENO, out, strnlen(out, 512));
+
+  va_end(args);
+#else
+  (void)fmt;
+  return;
+#endif
+}
--- libtimecontrol/src/log.h
+++ libtimecontrol/src/log.h
@@ -1,26 +1,4 @@
 #pragma once
 
-#include <mutex>
-#include <ostream>
-#include <stdarg.h>
-#include <stdio.h>
-#include <sstream>
-#include <thread>
 
-inline void log(const char* fmt, ...) {
-#ifdef DEBUG
-  static std::mutex log_mu;
-  std::lock_guard<std::mutex> l(log_mu);
-  va_list args;
-  va_start(args, fmt);
-  std::ostringstream oss;
-  oss << std::this_thread::get_id();
-  fprintf(stderr, "%s: ", oss.str().c_str());
-  vfprintf(stderr, fmt, args);
-  fprintf(stderr, "\n");
-  va_end(args);
-#else
-  (void)fmt;
-  return;
-#endif
-}
+void log(const char* fmt, ...);
